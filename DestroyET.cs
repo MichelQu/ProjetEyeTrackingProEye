4793980f9223d71f31e3b6821dbbb31848d6c9a8
MichelQu <59683373+MichelQu@users.noreply.github.com>
2020-08-26 16:25:25 +0200

    Add files via upload

---
+++ DestroyET.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.XR;
+using Tobii.G2OM;
+
+public class DestroyET : MonoBehaviour, IGazeFocusable
+{
+    bool targeted = false;
+    float timer = 0;
+
+    //The method of the "IGazeFocusable" interface, which will be called when this object receives or loses focus
+    public void GazeFocusChanged(bool hasFocus)
+    {
+        if (hasFocus) // S'il l'objet a le focus des yeux alors 
+        {
+            targeted = true;
+        }
+        else // Si l'objet a perdu le focus de l'oeil
+        {
+            targeted = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targeted)
+        {
+            // On incémente le temps 
+            timer += Time.deltaTime;
+            if (timer > 0.6f)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        else
+        {
+            timer = 0f;
+        }
+    }
+}
---
+++ TestEyeTracking.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.XR;
+
+public class TestEyeTracking : MonoBehaviour
+{
+    List<Vector3> ListPos = new List<Vector3>();
+    LineRenderer Trait;
+    Camera cam;
+
+    int freq = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        Trait = cam.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Get eye tracking data in world space
+        var eyeTrackingData = TobiiXR.GetEyeTrackingData(TobiiXR_TrackingSpace.World);
+
+        // Check if gaze ray is valid
+        if (eyeTrackingData.GazeRay.IsValid)
+        {
+            
+            // The origin of the gaze ray is a 3D point
+            var rayOrigin = eyeTrackingData.GazeRay.Origin;
+
+            // The direction of the gaze ray is a normalized direction vector
+            var rayDirection = eyeTrackingData.GazeRay.Direction;
+
+            Vector3 origine = rayOrigin;
+            Vector3 fin = rayOrigin + rayDirection * 10f;
+
+            Trait.SetPosition(0, origine);
+            Trait.SetPosition(1, fin);
+
+            // Debug.DrawLine(rayOrigin, rayOrigin + rayDirection * 10f);
+
+            #region Test Rendu Chemin du regard
+            // On affiche les coordonnées de Eye-tracking
+            // Debug.Log("Origine : " + rayOrigin);
+            // Debug.Log("Direction : " + rayDirection);
+            // freq += 1;
+            // ListPos.Add(rayOrigin + rayDirection * 10.0f);
+            //if (freq == 10)
+            //{
+            //    DrawLine();
+            //    freq = 0;
+            //}
+            #endregion
+        }
+    }
+
+    void DrawLine()
+    {
+        Trait.positionCount = ListPos.Count;
+        for (int i=0; i<ListPos.Count; i++)
+        {
+            Trait.SetPosition(i, ListPos[i]);
+        }
+    }
+}
